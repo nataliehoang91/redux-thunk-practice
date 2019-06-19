5aa50e980d127d5805cf1ade0c9d37cc9866ea3d
Nat <natalie.hoang91@gmail.com>
2019-06-19 15:12:22 +0700

    fetch post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
+export const FETCH_POST = 'FETCH_POST';
+
 export const fetchPost = ()=> async dispatch=>{
     const response = await jsonPlaceholder.get('./posts')
     
-    dispatch({type:'FETCH_POST',response:response})
-
+    dispatch({type:FETCH_POST,response:response})
 };
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {fetchPost} from "../actions";
+import { fetchPost } from "../actions";
 import {connect} from 'react-redux';
 
 class PostList extends React.Component{
-
     componentDidMount(){
-        this.props.fetchPost()
+        this.props.fetchPost();
     }
     render(){
+        console.log(this.props.posts)
         return (
           <div>
             <h3>Post List</h3>
@@ -16,5 +16,8 @@ class PostList extends React.Component{
     }
 }
 
+const mapStateToProps = (state)=> {
+    return {posts: state.posts}
+};
 
-export default connect(null,{fetchPost})(PostList);
+export default connect(mapStateToProps, {fetchPost})(PostList);
